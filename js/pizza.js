60ae2712de1af60634b3c53091c1afeeaa60e06c
Garrett Cintron <grrttcintron@gmail.com>
2020-09-22 10:06:15 -0700

    cleaned up code removed unecessary code

--- js/pizza.js
+++ js/pizza.js
@@ -1,19 +1,5 @@
 'use strict';
 //global variables
-Pizza.toppings = [];
-Pizza.cheese = [];
-Pizza.sauce = [];
-//pizza toppings
-var toppings = ['onions', 'mushrooms', 'spinach', 'roasted peppers', 'banana peppers', 'jalapeno peppers', 'tomatoes', 'black olives', 'pepperoni', 'sausage', 'beef', 'anchovy', 'chicken', 'shaved steak'];
-var cheese = ['American', 'Cheddar', 'Provolone', 'Swiss', 'Monterey Jack', 'feta', 'Havarti'];
-var sauces = ['Italian inspired robust red tomato', 'Alfredo', 'garlic Parmesan', 'Nacho cheese'];
-Pizza.toppings.push(toppings);
-Pizza.cheese.push(cheese);
-Pizza.sauce.push(sauces);
-console.log(Pizza.toppings);
-
-
-
 
 //constructor
 function Pizza(sauce, cheese, toppings) {
@@ -57,80 +43,20 @@ Pizza.prototype.removeCheese = function (cheese) {
     }
   }
 };
+
 //one pizza sauce not multiple
+//any sort of variable type can be passed into sauce keep aware
 Pizza.prototype.addSauce = function (sauce) {
   this.sauce = sauce;
 };
-
 Pizza.prototype.removeSauce = function () {
   this.sauce = 'olive oil';
 };
 //check to see if toppings are already on pizza
-console.log(toppings);
-
-
 //might not need to be here.
-Pizza.prototype.addTopping = function () {
-  var addToppings = document.getElementById('meats', 'vegetables');
-  var listEl = document.createElement('li');
-
-  for (let i = 0; i < Pizza.toppings[i].length; i++) {
-    listEl = Pizza.toppings[i];
-    addToppings.append(listEl);
-  }
-};
-
-Pizza.prototype.removeTopping = function () {
-  var removeToppings = document.getElementById('meats', 'vegetables');
-  var listEl = document.removeElement('li');
-
-  for (let i = 0; i < Pizza.toppings[i].length; i++) {
-    listEl = Pizza.toppings[i];
-    removeToppings.detach(listEl);
-  }//make it so u cant remove a topping that isnt there
-};
-
-//getElementById
-Pizza.prototype.addCheese = function () {
-  var addCheese = document.getElementById('cheese');
-  var listEl = document.createElement('li');
-  for (let i = 0; i < Pizza.cheese[i].length; i++) {
-    listEl = Pizza.cheese[i];
-    addCheese.append(listEl);
-  }
-};
-
-Pizza.prototype.removeCheese = function () {
-  var removeCheese = document.getElementById('cheese');
-  var listEl = document.removeElement('li');
-  for (let i = 0; i < Pizza.cheese[i].length; i++) {
-    listEl = Pizza.cheese[i];
-    removeCheese.detach(listEl);
-  }
-};
-
-Pizza.prototype.addSauce = function () {
-  var addSauce = document.getelementById('sauce');
-  var listEl = document.createElement('li');
-  for (let i = 0; i < Pizza.sauce[i].length; i++) {
-    listEl = Pizza.sauce[i];
-    addSauce.append(listEl);
-  }
-};
-
-Pizza.prototype.removeSauce = function () {
-  var removeSauce = document.getElementById('sauce');
-  var listEl = document.createElement('li');
-  for (let i = 0; i < Pizza.sauce[i].length; i++) {
-    listEl = Pizza.sauce[i];
-    removeSauce.detach(listEl);
-  }
-};
 
 Pizza.prototype.render = function () {
-
 };
-
 var sauceString = JSON.stringify(Pizza.sauce);
 var toppingsString = JSON.stringify(Pizza.toppings);
 var cheeseString = JSON.stringify(Pizza.cheese);
