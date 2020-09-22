4024aaf4b64a256d02b19414ffb09feab4a2a12d
Tahmina Ringer <tahmina.ringer@gmail.com>
2020-09-21 18:44:44 -0700

    added checkboxes to javaScript

--- js/order.js
+++ js/order.js
@@ -1,9 +1,15 @@
 'use strict';
 
-var toppings = ['green peppers', 'mushrooms', 'onions', 'fresh minced garlic', 'pinapple']; //Test toppings array
 
-function diplayYourPizza() {
-  var orderEl = document.getElementById('yourPizza'); //make sure we run ID name by Kim
+var sauces = ['red sauce', 'olive oil', 'bbq', 'pesto'];
+var basecheese = ['mozzarella', 'fresh mozzarella'];
+var extracheese = ['feta', 'gorgonzola', 'goat cheese', 'riccota'];
+var veggies = ['fresh minced garlic', 'mushrooms', 'green peppers','onions', 'red onions', 'olives', 'mama lil\'s', 'roasted garlic', 'sundried tomatoes', 'pinapple', 'jalepeno', 'pepperoncini']; //Test toppings array
+var meats = ['canadian bacon', 'salami', 'prosciutto', 'pepperoni', 'chicken', 'sausage', 'anchovies'];
+var afterbakes = ['extra virgin olive oil', 'parsley', 'basil', 'arugula', 'tomatoes', 'sea salt'];
+
+function diplayYourPizza(toppings) {
+  var orderEl = document.getElementById('your_pizza'); //make sure we run ID name by Kim
   var toppingsEl = document.createElement('ul');
   orderEl.append(toppingsEl);
 
@@ -19,7 +25,7 @@ function diplayYourPizza() {
 
   }
 }
-diplayYourPizza();
+diplayYourPizza(['green peppers', 'olives']);
 
 function toppingLayer(topping) {
   switch(topping) {
@@ -28,9 +34,47 @@ function toppingLayer(topping) {
   case 'mushrooms': return 11;
   case 'onions': return 13;
   case 'fresh minced garlic': return -1;
+  default: return 0;
   }
 }
 
 function sortToppings(toppings) {
   toppings.sort(function(a,b){return toppingLayer(a) - toppingLayer(b);});
 }
+
+function createCheckboxList(toppings, container) {
+  for (let i = 0; i < toppings.length; i++) {
+    var inputEl = document.createElement('imput');
+    var labelEl = document.createElement('label');
+    container.append(inputEl); //do we want to append the input to the label?
+    container.append(labelEl);
+    container.append(document.createElement('br'));
+
+
+    inputEl.type = 'checkbox';
+    inputEl.id = 'cb_' + toppings[i];
+    inputEl.name = 'cb_' + toppings[i];
+    inputEl.value = toppings[i];
+    labelEl.for = 'cb_' + toppings[i];
+    labelEl.textContent = toppings[i];
+
+  }
+
+  return container;
+}
+
+function createAllCheckBoxes() {
+  var sauceEl = document.getElementById('sauce');
+  var cheeseEl = document.getElementById('cheese');
+  var veggiesEl = document.getElementById('vegetables');
+  var meatsEl = document.getElementById('meats');
+  // var afterbakesEl = document.getElementById('afterbakes');
+  createCheckboxList(sauces, sauceEl);
+  createCheckboxList(basecheese, cheeseEl);
+  createCheckboxList(veggies.concat(extracheese), veggiesEl); //should these be separated or copied?
+  createCheckboxList(meats, meatsEl);
+  // createCheckboxList(afterbakes, afterbakesEl);
+}
+
+createAllCheckBoxes();
+
