71ac9f5d55e024c7fca07ab03f15463ceb29658d
RYAN PILON <Ryanpilon1995@gmail.com>
2020-09-24 15:20:53 -0700

    fixed syntax errors

--- js/ticket.js
+++ js/ticket.js
@@ -11,13 +11,12 @@ pizzaOrder();
 function commentButtonHandler(event) {
   event.preventDefault();
   var commentPlaceEl = document.createElement('p');
-  commentPlaceEl.id = "instruction"
-  var textFieldEl = document.getElementById('text_box')
-  commentPlaceEl.textContent = textFieldEl.value
+  commentPlaceEl.id = "instruction";
+  var textFieldEl = document.getElementById('text_box');
+  commentPlaceEl.textContent = textFieldEl.value;
   var orderPizzaEl = document.getElementById('order_pizza');
   orderPizzaEl.append(commentPlaceEl);
-  document.getElementById('comment').innerHTML = null
-    
+  document.getElementById('comment').innerHTML = null;
 }
 
 document.getElementById('submit_button').addEventListener('click', commentButtonHandler);
